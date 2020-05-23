3aa3dbf0913f5a85a6c74c9616f67478d1c27b63
zixueheng <356126067@qq.com>
2020-05-23 11:47:43 +0800

    update routes

--- routes/wapapi.go
+++ routes/wapapi.go
@@ -10,8 +10,8 @@ import (
 
 // InitWapAPI 初始化 WapApi 模块路由
 func InitWapAPI(app iris.Party) {
-	adminParty := app.Party("/wapapi", middleware.CrsAuth()).AllowMethods(iris.MethodOptions)
-	mvc.Configure(adminParty, loadWapAPIController)
+	wapParty := app.Party("/wapapi", middleware.CrsAuth()).AllowMethods(iris.MethodOptions)
+	mvc.Configure(wapParty, loadWapAPIController)
 }
 
 // loadWapAPIController 加载 wapapi 控制器
