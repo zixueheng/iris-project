b73ff1b319c1d12d3d81601cfb1e781c09aa4780
zixueheng <356126067@qq.com>
2020-07-04 14:21:23 +0800

    add http request

--- go.mod
+++ go.mod
@@ -3,45 +3,15 @@ module iris-project
 go 1.14
 
 require (
-	github.com/ajg/form v1.5.1 // indirect
-	github.com/fasthttp-contrib/websocket v0.0.0-20160511215533-1f3b11f56072 // indirect
 	github.com/go-playground/locales v0.13.0
 	github.com/go-playground/universal-translator v0.17.0
-	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/go-playground/validator/v10 v10.3.0
-	github.com/go-redis/redis/v7 v7.3.0
-	github.com/google/go-querystring v1.0.0 // indirect
-	github.com/gorilla/schema v1.1.0 // indirect
-	github.com/howeyc/fsnotify v0.9.0 // indirect
-	github.com/imkira/go-interpol v1.1.0 // indirect
-	github.com/iris-contrib/formBinder v5.0.0+incompatible // indirect
+	github.com/go-redis/redis/v7 v7.4.0
 	github.com/iris-contrib/middleware/cors v0.0.0-20191219204441-78279b78a367
 	github.com/iris-contrib/middleware/jwt v0.0.0-20191219204441-78279b78a367
 	github.com/jameskeane/bcrypt v0.0.0-20120420032655-c3cd44c1e20f
 	github.com/jinzhu/configor v1.2.0
-	github.com/jinzhu/gorm v1.9.12
-	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
-	github.com/kataras/iris v11.1.1+incompatible
+	github.com/jinzhu/gorm v1.9.14
 	github.com/kataras/iris/v12 v12.1.8
-	github.com/klauspost/compress v1.10.7 // indirect
-	github.com/mattn/go-colorable v0.1.6 // indirect
-	github.com/moul/http2curl v1.0.0 // indirect
-	github.com/nats-io/jwt v1.0.1 // indirect
-	github.com/nats-io/nats-server/v2 v2.1.7 // indirect
-	github.com/nats-io/nkeys v0.2.0 // indirect
-	github.com/sergi/go-diff v1.1.0 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
-	github.com/silenceper/gowatch v0.0.0-20200530075017-a42b5eca00f9 // indirect
-	github.com/silenceper/log v0.0.0-20171204144354-e5ac7fa8a76a // indirect
-	github.com/smartystreets/goconvey v1.6.4 // indirect
-	github.com/valyala/fasthttp v1.14.0 // indirect
-	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
-	github.com/yalp/jsonpath v0.0.0-20180802001716-5cc68e5049a0 // indirect
-	github.com/yudai/gojsondiff v1.0.0 // indirect
-	github.com/yudai/golcs v0.0.0-20170316035057-ecda9a501e82 // indirect
-	github.com/yudai/pp v2.0.1+incompatible // indirect
-	golang.org/x/crypto v0.0.0-20200602180216-279210d13fed // indirect
-	golang.org/x/sys v0.0.0-20200602225109-6fdc65e7d980 // indirect
-	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
-	gopkg.in/yaml.v1 v1.0.0-20140924161607-9f9df34309c0 // indirect
 )
---
+++ lib/request/request.go
@@ -0,0 +1,167 @@
+package request
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+	"sync"
+)
+
+// 常量
+const (
+	// 请求类型
+	GetMethod    = "GET"
+	PostMethod   = "POST"
+	DeleteMethod = "DELETE"
+	PutMethod    = "PUT"
+
+	// 请求 content-type
+	FormType = "form"
+	JSONType = "json"
+)
+
+// HTTPRequest HTTP请求结构体
+type HTTPRequest struct {
+	Link        string
+	ContentType string
+	Header      map[string]string
+	Body        map[string]string
+	sync.RWMutex
+}
+
+// NewHTTPRequest 新建一个HTTP请求
+func NewHTTPRequest(link string) *HTTPRequest {
+	return &HTTPRequest{
+		Link:        link,
+		ContentType: FormType,
+	}
+}
+
+// SetBody 设置请求Body
+func (h *HTTPRequest) SetBody(body map[string]string) {
+	h.Lock()
+	defer h.Unlock()
+	h.Body = body
+}
+
+// SetHeader 设置请求Header
+func (h *HTTPRequest) SetHeader(header map[string]string) {
+	h.Lock()
+	defer h.Unlock()
+	h.Header = header
+}
+
+// SetContentType 设置请求Content类型
+func (h *HTTPRequest) SetContentType(contentType string) {
+	h.Lock()
+	defer h.Unlock()
+	h.ContentType = contentType
+}
+
+// Get 发送GET请求
+func (h *HTTPRequest) Get() ([]byte, error) {
+	return h.send(GetMethod)
+}
+
+// Post 发送POST请求
+func (h *HTTPRequest) Post() ([]byte, error) {
+	return h.send(PostMethod)
+}
+
+// Put 发送Put请求
+func (h *HTTPRequest) Put() ([]byte, error) {
+	return h.send(PutMethod)
+}
+
+// Delete 发送Delete请求
+func (h *HTTPRequest) Delete() ([]byte, error) {
+	return h.send(DeleteMethod)
+}
+
+// URLBuild 构建URL
+func URLBuild(link string, data map[string]string) string {
+	u, _ := url.Parse(link)
+	q := u.Query()
+	for k, v := range data {
+		q.Set(k, v)
+	}
+	u.RawQuery = q.Encode()
+	return u.String()
+}
+
+// send 执行请求
+func (h *HTTPRequest) send(method string) ([]byte, error) {
+	var (
+		req      *http.Request
+		resp     *http.Response
+		client   http.Client
+		sendData string
+		err      error
+	)
+
+	if len(h.Body) > 0 {
+		if strings.ToLower(h.ContentType) == JSONType {
+			sendBody, jsonErr := json.Marshal(h.Body)
+			if jsonErr != nil {
+				return nil, jsonErr
+			}
+			sendData = string(sendBody)
+		} else {
+			sendBody := http.Request{}
+			sendBody.ParseForm()
+			for k, v := range h.Body {
+				sendBody.Form.Add(k, v)
+			}
+			sendData = sendBody.Form.Encode()
+		}
+	}
+
+	//忽略https的证书
+	client.Transport = &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	req, err = http.NewRequest(method, h.Link, strings.NewReader(sendData))
+	if err != nil {
+		return nil, err
+	}
+	defer req.Body.Close()
+
+	//设置默认header
+	if len(h.Header) == 0 {
+		//json
+		if strings.ToLower(h.ContentType) == JSONType {
+			h.Header = map[string]string{
+				"Content-Type": "application/json; charset=utf-8",
+			}
+		} else { //form
+			h.Header = map[string]string{
+				"Content-Type": "application/x-www-form-urlencoded",
+			}
+		}
+	}
+
+	for k, v := range h.Header {
+		if strings.ToLower(k) == "host" {
+			req.Host = v
+		} else {
+			req.Header.Add(k, v)
+		}
+	}
+
+	resp, err = client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("error http code :%d", resp.StatusCode) //errors.New(fmt.Sprintf("error http code :%d", resp.StatusCode))
+	}
+
+	return ioutil.ReadAll(resp.Body)
+}
