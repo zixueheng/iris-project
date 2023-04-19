bb6b9e2e26b77b4aa5b4e37fc0182938bc1a5e77
zixueheng <356126067@qq.com>
2023-04-19 15:03:25 +0800

    21

--- lib/mongodb/test/test.go
+++ lib/mongodb/test/test.go
@@ -4,45 +4,42 @@
  * @Email: 356126067@qq.com
  * @Phone: 15215657185
  * @Date: 2023-03-13 10:27:54
- * @LastEditTime: 2023-03-15 11:30:15
+ * @LastEditTime: 2023-04-19 14:08:01
  */
 package main
 
-import (
-	"context"
-	"log"
-	"time"
-
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
-)
+// "go.mongodb.org/mongo-driver/bson"
+// "go.mongodb.org/mongo-driver/mongo"
+// "go.mongodb.org/mongo-driver/mongo/options"
+// "go.mongodb.org/mongo-driver/mongo/readpref"
 
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	// defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://root:123@localhost:27017/"))
-	if err != nil {
-		panic(err)
-	}
-
-	defer func() {
-		if err = client.Disconnect(ctx); err != nil {
+	/*
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		// defer cancel()
+		client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://root:123@localhost:27017/"))
+		if err != nil {
 			panic(err)
 		}
-		cancel()
-	}()
 
-	// ctx, cancel = context.WithTimeout(context.Background(), 2*time.Second)
-	// defer cancel()
-	err = client.Ping(ctx, readpref.Primary())
+		defer func() {
+			if err = client.Disconnect(ctx); err != nil {
+				panic(err)
+			}
+			cancel()
+		}()
 
-	collection := client.Database("test").Collection("numbers")
+		// ctx, cancel = context.WithTimeout(context.Background(), 2*time.Second)
+		// defer cancel()
+		err = client.Ping(ctx, readpref.Primary())
 
-	// ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
-	// defer cancel()
-	res, err := collection.InsertOne(ctx, bson.D{{"name", "pi"}, {"value", 3.14159}})
-	id := res.InsertedID
-	log.Println(id)
+		collection := client.Database("test").Collection("numbers")
+
+		// ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+		// defer cancel()
+		res, err := collection.InsertOne(ctx, bson.D{{"name", "pi"}, {"value", 3.14159}})
+		id := res.InsertedID
+		log.Println(id)
+	*/
+
 }
